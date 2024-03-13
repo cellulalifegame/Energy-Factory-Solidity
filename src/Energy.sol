9be5051fe3471eb44fd1911d17c26d7b5be1a208
dabotxt <1095450045@qq.com>
2024-03-13 18:22:14 +0800

    feat(md): add md

--- src/Energy.sol
+++ src/Energy.sol
@@ -5,7 +5,7 @@ contract Energy {
 
     event ClaimEnergy(address account, uint256 claimTime);
 
-    function sendClaimEnergyRequest(address account) public {
+    function sendClaimEnergyRequest() public {
         uint256 claimTime = block.timestamp;
         emit ClaimEnergy(msg.sender, claimTime);
     }
